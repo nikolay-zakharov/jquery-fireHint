3fdbfb2c846e9db797899cc9bd28854e042c75a3
Nikolay Zakharov <nickolay.zakharov@gmail.com>
2013-12-17 22:35:22 +0400

    Events added: close, pushpin; default event listener for `selectstart` was took off

--- jquery-fireHint.js
+++ jquery-fireHint.js
@@ -16,16 +16,8 @@
 			timeout: 10,
 			padding: 18,
 			position: 'tr',
-			css: {
-				'.firehint-message': {},
-				'.firehint-message-header': {},
-				'.firehint-message-body': {}
-			},
-			events: {
-				'.firehint-message': {
-					selectstart: function(evt){ evt.preventDefault(); }
-				}
-			}
+			css: {},
+			events: {}
 		};
 	
 	var redrawTimeout;
@@ -34,17 +26,26 @@
 		var config = $.extend(true, null, defaultConfig, config);
 
 		var reorganizeHints = function(){
-			var topCurrent = $(document).scrollTop();
-
+			var topCurrent = $(document).scrollTop()+4;
+			
 			$(hints).finish().css({left:0});
 			
 			for(var ind=1; ind<=hints.length; ind++){
 				var leftCurrent = $(window).width()-$(hints[hints.length-ind]).width() - 30;
 
 				$(hints[hints.length-ind]).css({zIndex:10000-ind}).animate({position:'absolute', top:topCurrent+'px', left:leftCurrent+'px'});
-
 				topCurrent += $(hints[hints.length-ind]).height()+config.padding;
 			}
+		};
+
+		var closeHint = function(hint){
+			hints = hints.filter(function(item){
+				if(item==hint)
+					$(hint).remove();
+
+				return item!=hint;
+			});
+			reorganizeHints();
 		};
 		
 		$(window).on('resize', function(evt){
@@ -62,36 +63,36 @@
 		var currentHint = $(document.createElement('div')).addClass('firehint-message').css({opacity:0.9}).append([
 			$(document.createElement('div')).addClass('firehint-message-header').append(config.elements.header),
 			$(document.createElement('div')).addClass('firehint-message-body').append(config.elements.body)
-		]);
+		]),
+			currentHint_timeout = null;
 		
-		// Overloading css
-		Object.keys(config.css).forEach(function(xpath){
-			(currentHint.is(xpath) ? currentHint : currentHint.find(xpath)).css(config.css[xpath]);
-		});
+		currentHint.on('close',function(evt){ closeHint(currentHint); });
+		currentHint.on('pushpin',function(evt){ if(currentHint_timeout != null) window.clearTimeout(currentHint_timeout); });
 		
-		// Binding events
-		Object.keys(config.events).forEach(function(xpath){
-			Object.keys(config.events[xpath]).forEach(function(eventName){
-				(currentHint.is(xpath) ? currentHint : currentHint.find(xpath))
-					.on(eventName, config.events[xpath][eventName]);
-			});
-		});
+		$('.firehint-message-close', currentHint).on('click', function(){ closeHint(currentHint); });
 
 		// Run hint
 		hints.push(currentHint);
 		$('body').append(currentHint);
+
+		// Overloading css
+		Object.keys(config.css).forEach(function(xpath){
+			currentHint.parents(':first').find(xpath).filter(function(ind,item){ return $(item).closest(currentHint).length>0; })
+				.css(config.css[xpath]);
+		});
+
+		// Binding events
+		Object.keys(config.events).forEach(function(xpath){
+			Object.keys(config.events[xpath]).forEach(function(eventName){
+				currentHint.parents(':first').find(xpath).filter(function(ind,item){ return $(item).closest(currentHint).length>0; })
+					.on(eventName, config.events[xpath][eventName]);
+			});
+		});
+		
 		reorganizeHints();
 
 		// Close hint on timeout
-		window.setTimeout(function(){
-			hints = hints.filter(function(item){
-				if(item==currentHint){
-					$(currentHint).remove();
-					return false;
-				}else
-					return true;
-			});
-			reorganizeHints();
-		}, config.timeout*1000);
+		if(config.timeout>0)
+			currentHint_timeout = window.setTimeout(function(){ closeHint(currentHint); }, config.timeout*1000);
 	}
 })(jQuery);
